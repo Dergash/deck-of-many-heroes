79b95ceb52c3433e37169dfd6683e41aa0bea1d9
dergash <dergachev@hotmail.com>
2022-01-15 20:00:18 +0300

    feat(arcanum-ruleset): fire spells

---
+++ features/arcanum/spells/fire.ts
@@ -0,0 +1,48 @@
+import { ArcanumSpell } from '../ruleset'
+
+export const FireSpells: ArcanumSpell[] = [
+    {
+        name: 'Agility of Fire',
+        college: 'Fire',
+        rank: 1,
+        level: 1,
+        energy: 5,
+        energyConsumption: 1,
+        willpower: 6
+    },
+    {
+        name: 'Wall of Fire',
+        college: 'Fire',
+        rank: 2,
+        level: 1,
+        energy: 10,
+        energyConsumption: 1,
+        willpower: 9
+    },
+    {
+        name: 'Fireflash',
+        college: 'Fire',
+        rank: 3,
+        level: 5,
+        energy: 15,
+        willpower: 12
+    },
+    {
+        name: 'Body of Fire',
+        college: 'Fire',
+        rank: 4,
+        level: 10,
+        energy: 25,
+        energyConsumption: 10,
+        willpower: 15
+    },
+    {
+        name: 'Call Fire Elemental',
+        college: 'Fire',
+        rank: 5,
+        level: 15,
+        energy: 50,
+        energyConsumption: 5,
+        willpower: 18
+    }
+]
