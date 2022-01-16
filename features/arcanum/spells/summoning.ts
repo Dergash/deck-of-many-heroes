16414fc75937c3a7ad0277d8bb77ebcb15678b69
dergash <dergachev@hotmail.com>
2022-01-16 21:39:56 +0300

    feat(arcanum-ruleset): summoning spells

---
+++ features/arcanum/spells/summoning.ts
@@ -0,0 +1,48 @@
+import { ArcanumSpell } from '../ruleset'
+
+export const SummoningSpells: ArcanumSpell[] = [
+    {
+        name: 'Plague of Insects',
+        college: 'Summoning',
+        rank: 1,
+        level: 1,
+        energy: 3,
+        energyConsumption: 1,
+        willpower: 6
+    },
+    {
+        name: 'Orcish Champion',
+        college: 'Summoning',
+        rank: 2,
+        level: 1,
+        energy: 10,
+        energyConsumption: 2,
+        willpower: 9
+    },
+    {
+        name: 'Guardian Ogre',
+        college: 'Summoning',
+        rank: 3,
+        level: 5,
+        energy: 15,
+        energyConsumption: 3,
+        willpower: 12
+    },
+    {
+        name: 'Hellgate',
+        college: 'Summoning',
+        rank: 4,
+        level: 10,
+        energy: 30,
+        energyConsumption: 6,
+        willpower: 15
+    },
+    {
+        name: 'Familiar',
+        college: 'Summoning',
+        rank: 5,
+        level: 15,
+        energy: 60,
+        willpower: 18
+    }
+]
